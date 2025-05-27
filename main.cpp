bbfc2344d0b55f57725157e4caf86a840becfd4e
Rishav_Giri <rishavgiri509@gmail.com>
2025-05-27 11:48:25 +0530

    maded the back none of the sysytem and now need to add the features and functioning

--- main.cpp
+++ main.cpp
@@ -1,65 +1,63 @@
 #include "raylib.h"
+#include "rlgl.h"
+#include "raymath.h"
 #include <iostream>
 using namespace std;
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
+    int screen = 2;
 
-    InitWindow(screenWidth, screenHeight, "raylib [text] example - font loading");
-
-    // Define characters to draw
-    // NOTE: raylib supports UTF-8 encoding, following list is actually codified as UTF8 internally
-    const char msg[256] = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHI\nJKLMNOPQRSTUVWXYZ";
-
-
-    // TTF font : Font data and atlas are generated directly from TTF
-    // NOTE: We define a font base size of 32 pixels tall and up-to 250 characters
-    Font fontTtf = LoadFontEx("Resource/Movistar Text Regular.ttf", 32, 0, 512);
-    if(IsFontValid(fontTtf)){cout<<"######################################################"<<endl;}
-    else{cout<<"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;}
-    SetTextLineSpacing(16);         // Set line spacing for multiline text (when line breaks are included '\n')
-
-    bool useTtf = false;
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    InitWindow(screenWidth, screenHeight, "Main File");
+    Camera2D camera = { 0 };
+    camera.zoom = 1.0f;
+    SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_SPACE)) useTtf = true;
-        else useTtf = false;
-        //----------------------------------------------------------------------------------
-
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
-
-            DrawText("Hold SPACE to use TTF generated font", 20, 20, 20, LIGHTGRAY);
-
-            if (!useTtf)
-            {
-                DrawTextEx(fontTtf, msg, (Vector2){ 20.0f, 100.0f }, (float)fontTtf.baseSize, 2, LIME);
-                DrawText("Using TTF font generated", 20, GetScreenHeight() - 30, 20, GRAY);
+             BeginMode2D(camera);
+             DrawRectangleRec(Rectangle {700,200,600,800}, Color {0,0,255,255}); // edit mode
+             if(IsKeyDown(KEY_LEFT_CONTROL) && IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
+                Vector2 delta = GetMouseDelta();
+                delta = Vector2Scale(delta, -1.0f/camera.zoom);
+                camera.target = Vector2Add(camera.target, delta);
+             }else{
+                camera.target.y += 0-(GetMouseWheelMove()*30);
+             }
+             EndMode2D();
+            
+            if(screen == 1){
+                DrawRectangleRec(Rectangle {0,35,1950,35}, Color {0,255,0,255}); //top
+                DrawRectangleRec(Rectangle {0,0,1950,35}, Color {255,0,0,255}); // bottom
+                
+                //temp ----
+                for (int y = 0; y < 10; y++){
+                    for (int x = 0; x < 8; x++){ // vector<vector<Rectangle>> 
+                        DrawRectangleRec(Rectangle {(75*x+150*x)+75,(125*y+200*y)+125,150,200}, Color {0,0,255,255});
+                    }
+                }
+            }else if(screen == 2){
+                DrawRectangleRec(Rectangle {0,0,1950,35}, Color {255,0,0,255}); // top
+                DrawRectangleRec(Rectangle {0,35,1950,35}, Color {0,255,0,255}); //bottom
+                
+                DrawRectangleRec(Rectangle {0,75,300,900}, Color {255,0,0,255}); // tool bar
+                
             }
-
+ 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadFont(fontTtf);    // TTF Font unloading
 
     CloseWindow();          // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
