55fc36b6aef068c9a871914e3dde6b74b1aa4dc0
Rishav_Giri <rishavgiri509@gmail.com>
2025-05-28 11:35:01 +0530

    made ui improvements and added a template managment sysytem currentyl hanndles just rectangles but will work on more later

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "raylib.h"
+#include "screen.h"
 #include "rlgl.h"
 #include "raymath.h"
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <string>
 using namespace std;
-
+Rectangle MakeRectangle(string s);
 int main(void)
 {
     // Initialization
@@ -11,51 +16,52 @@ int main(void)
     const int screenWidth = GetScreenWidth();
     const int screenHeight = GetScreenHeight();
     int screen = 2;
+    vector<Rectangle> Templates;
 
     InitWindow(screenWidth, screenHeight, "Main File");
     Camera2D camera = { 0 };
     camera.zoom = 1.0f;
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
-
+    
+    ifstream templates("templates.txt");
+    string s;
+    while(getline(templates, s)){
+        Rectangle rect = MakeRectangle(s); // convert x-y-width-height- from string to rectangle;
+        Templates.push_back(rect);
+    }
+    templates.close();
+    
+    //permanents
+    Rectangle Exit = {1870,3,60,30};
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-
-            ClearBackground(RAYWHITE);
-             BeginMode2D(camera);
-             DrawRectangleRec(Rectangle {700,200,600,800}, Color {0,0,255,255}); // edit mode
-             if(IsKeyDown(KEY_LEFT_CONTROL) && IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
-                Vector2 delta = GetMouseDelta();
-                delta = Vector2Scale(delta, -1.0f/camera.zoom);
-                camera.target = Vector2Add(camera.target, delta);
-             }else{
-                camera.target.y += 0-(GetMouseWheelMove()*30);
-             }
-             EndMode2D();
+            ClearBackground(Color {180,180,180,255});
             
-            if(screen == 1){
-                DrawRectangleRec(Rectangle {0,35,1950,35}, Color {0,255,0,255}); //top
-                DrawRectangleRec(Rectangle {0,0,1950,35}, Color {255,0,0,255}); // bottom
+            if(screen == 1){// Screen 1 is being loaded onto the screen
                 
-                //temp ----
-                for (int y = 0; y < 10; y++){
-                    for (int x = 0; x < 8; x++){ // vector<vector<Rectangle>> 
-                        DrawRectangleRec(Rectangle {(75*x+150*x)+75,(125*y+200*y)+125,150,200}, Color {0,0,255,255});
-                    }
-                }
+                screen1(Exit, Templates);
+                
             }else if(screen == 2){
-                DrawRectangleRec(Rectangle {0,0,1950,35}, Color {255,0,0,255}); // top
-                DrawRectangleRec(Rectangle {0,35,1950,35}, Color {0,255,0,255}); //bottom
                 
-                DrawRectangleRec(Rectangle {0,75,300,900}, Color {255,0,0,255}); // tool bar
-                
+                screen2 (Exit, camera);
+   
             }
- 
+            
+        if(CheckCollisionPointRec(GetMousePosition(), Exit)){
+            DrawRectangleRec(Exit, Color {0,0,0,50});
+            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+                CloseWindow();
+            }
+        }
+
         EndDrawing();
+        
         //----------------------------------------------------------------------------------
     }
 
@@ -63,4 +69,30 @@ int main(void)
     //--------------------------------------------------------------------------------------
 
     return 0;
+}
+
+Rectangle MakeRectangle(string str){
+    Rectangle temp = {0,0,0,0};
+    vector<int> temprect;
+    string rect;
+    for(int i=0; i <= int(str.length()); i++){
+        if(str[i] != '-'){
+            rect.push_back(str[i]);
+        }else if(str[i] == '\n'){
+            temprect.push_back(stoi(rect));
+            temp.x = temprect[0];
+            temp.y = temprect[1];
+            temp.width = temprect[2];
+            temp.height = temprect[3];
+            return temp;
+        }else{
+            temprect.push_back(stoi(rect));
+            rect.clear();
+        }
+    }
+    temp.x = temprect[0];
+    temp.y = temprect[1];
+    temp.width = temprect[2];
+    temp.height = temprect[3];
+    return temp;
 }
